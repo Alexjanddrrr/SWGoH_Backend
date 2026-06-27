4db4f08280dfa7591d1341a06585c15fd66fe403
Alexandr Karateev <karateevx@mail.ru>
2026-06-27 12:25:12 +0300

    some rename

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
-from Endpoints.get_player_units import all_units
+from Endpoints.player_units import all_units
 from Endpoints.cal_kestis_jedi import cal_kestis
-from database import db, Player
+from database import db, Player, Characters
 
 app = Flask(__name__)
 app.register_blueprint(all_units)
 app.register_blueprint(cal_kestis)
 
 with db:
-    db.create_tables([Player])
+    db.create_tables([Player, Characters])
 
 
 app.run()
