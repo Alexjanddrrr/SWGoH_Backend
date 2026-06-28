650c2d6c159655df9b8c2041390d20e5eccc69ff
Alexandr Karateev <karateevx@mail.ru>
2026-06-28 14:09:26 +0300

    add fleet table

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from Endpoints.player_units import all_units
 from Endpoints.cal_kestis_jedi import cal_kestis
-from database import db, Player, Characters
+from database import db, Player, Characters, Fleet
 
 app = Flask(__name__)
 app.register_blueprint(all_units)
 app.register_blueprint(cal_kestis)
 
 with db:
-    db.create_tables([Player, Characters])
+    db.create_tables([Player, Characters, Fleet])
 
 
 app.run()
