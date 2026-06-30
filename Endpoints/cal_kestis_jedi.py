1aeb2c1045772bdf46f85ac5d3f448b192952ee6
Alexandr Karateev <karateevx@mail.ru>
2026-06-30 13:16:54 +0300

    add check cal jedi

--- Endpoints/cal_kestis_jedi.py
+++ Endpoints/cal_kestis_jedi.py
@@ -1,17 +1,21 @@
-from flask import request, Blueprint
-import requests
+from flask import Blueprint
+from database import Player, Characters
 
 cal_kestis = Blueprint('cal_kestis_jedi', __name__)
 
 
-@cal_kestis.route('/cal_jedi_check', methods=['GET'])
+@cal_kestis.route('/all/cal_jedi/', methods=['GET'])
 def cal_kestis_jedi():
-    url = 'COMLINK_URL/player'
-    get_ally_code = request.args.get('allycode')
-    payload = {
-        "payload": {
-            "allyCode": f'{get_ally_code}',
-        },
-        "enums": False
-    }
-    response = requests.post(url, json=payload)
+    """
+    Собирает словарь с игроком, персонажем и его уровнем реликвий
+
+    :return: Словарь с игроками и персонажем и уровнем реликвий
+
+    """
+    output_dict = {}
+
+    for char in Characters.select().where(Characters.unit.ilike('JEDIKNIGHTCAL%')):
+        player_nick = Player.get(Player.ally_code == char.ally_id).nickname
+        output_dict.setdefault(player_nick, {char.unit: char.relic})
+
+    return output_dict
