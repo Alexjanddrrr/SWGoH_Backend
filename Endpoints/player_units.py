5f2a3459147b36eab3f4d7c769ed20c2e6d38904
Alexandr Karateev <karateevx@mail.ru>
2026-06-30 13:20:22 +0300

    moved registration to another module

--- Endpoints/player_units.py
+++ Endpoints/player_units.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 import requests
-from database import Player, Characters
+from database import Player, Characters, Fleet
+from Utils.register_player import player_registration
 
 all_units = Blueprint('units', __name__)
 
@@ -15,8 +16,13 @@ def units_info(ally_code: str):
         ally_code: Код союзника
     :return: Если игрок уже есть в базе, то возвращает список юнитов с уровнем реликвий.
     Если нет, то регистрирует
+
     """
     url = 'COMLINK_URL/player'
+    output_dict = {
+        'UNITS': {},
+        'FLEET': []
+    }
     payload = {
         "payload": {
             "allyCode": f'{ally_code}',
@@ -27,25 +33,13 @@ def units_info(ally_code: str):
     response = requests.post(url, json=payload)
     player_data = response.json()['rosterUnit']
     player_nickname = response.json()['name']
+    player = Player.get_or_none(nickname=player_nickname, ally_code=ally_code)
 
-    output_dict = {}
-    if Player.get_or_none(nickname=player_nickname, ally_code=ally_code):
-        for char in Characters.select().where(Characters.ally_id == ally_code):
-            output_dict[char.unit] = char.relic
-        return output_dict
+    if player is None:
+        player_registration(player_nickname, ally_code, player_data)
 
-    else:
-        Player.create(nickname=player_nickname, ally_code=ally_code)
-        output = []
-        for some_data in player_data:
-            try:
-                if some_data['currentTier'] < 13:
-                    output.append(f'UNIT: {some_data['definitionId']}, GEAR LVL: {some_data['currentTier']}')
-                else:
-                    output.append(f'UNIT: {some_data['definitionId']}, RELIC LVL: {some_data['relic']['currentTier'] - 2}')
-                    Characters.create(ally_id=ally_code,
-                                          unit=f'{some_data['definitionId']}',
-                                          relic=f'{some_data['relic']['currentTier'] - 2}')
-            except TypeError as exc:
-                output.append(f'FLEET UNIT: {some_data['definitionId']}')
-        return output
+    for char in Characters.select().where(Characters.ally_id == ally_code):
+        output_dict['UNITS'][char.unit] = char.relic
+    for ships in Fleet.select().where(Fleet.ally_id == ally_code):
+        output_dict['FLEET'].append(ships.ship)
+    return output_dict
