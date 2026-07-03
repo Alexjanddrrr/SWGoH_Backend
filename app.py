633ad4cd3bd9fa0e432afcb81d3744f6707782c2
Alexandr Karateev <karateevx@mail.ru>
2026-07-03 13:36:25 +0300

    add background task

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 from Endpoints.player_units import all_units
 from Endpoints.cal_kestis_jedi import cal_kestis
 from database import db, Player, Characters, Fleet
+from Utils.update_units import background_task
+
+
 
 app = Flask(__name__)
 app.register_blueprint(all_units)
@@ -10,5 +13,6 @@ app.register_blueprint(cal_kestis)
 with db:
     db.create_tables([Player, Characters, Fleet])
 
-
-app.run()
+if __name__ == '__main__':
+    background_task()
+    app.run(use_reloader=False)
